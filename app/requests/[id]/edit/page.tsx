6789c30badffc8a1e05a4f37eef78f847a44e15d
aaronmathew-spec <aaron.mathew@gmail.com>
2025-09-19 21:46:47 -0400

    Update page.tsx

--- app/requests/[id]/edit/page.tsx
+++ app/requests/[id]/edit/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
-import { supabase } from '../../../../lib/supabaseClient';
+import supabase from '../../lib/supabaseClient';
 
 export default function EditRequest() {
   const { id } = useParams<{ id: string }>();
