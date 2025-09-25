56850bc4ba51696f7a7b0c7c2b49200507a95750
aaronmathew-spec <aaron.mathew@gmail.com>
2025-09-24 21:02:43 -0400

    Create supabaseServer.ts
    chore: add supabase server helper

---
+++ lib/supabaseServer.ts
@@ -0,0 +1,16 @@
+import { cookies } from "next/headers";
+import { createServerClient } from "@supabase/ssr";
+
+export function getServerSupabase() {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get: (name: string) => cookies().get(name)?.value,
+        set() {},
+        remove() {},
+      },
+    }
+  );
+}
